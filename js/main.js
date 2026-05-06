846d9f6be456e273920d416eb130b2a8949e6634
Carlos Eduardo Fonseca Dantas <108951272+carlinhosdantass@users.noreply.github.com>
2026-05-06 00:45:42 +0000

    fix: correção do footer/header e trasnferência do index para a raiz

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
 function loadContent(file, div) {
     fetch(file)
         .then(function(response) {
+            if (!response.ok) {
+                throw new Error("Erro ao carregar: " + file);
+            }
             return response.text();
         })
         .then(function(html) {
             document.getElementById(div).innerHTML = html;
+        })
+        .catch(function(error) {
+            console.error(error);
         });
 }
 
 window.onload = function() {
-    loadContent('components/header.html', 'header-placeholder');
-    loadContent('components/footer.html', 'footer-placeholder');
+    loadContent('html/components/header.html', 'header-placeholder');
+    loadContent('html/components/footer.html', 'footer-placeholder');
 };
 
 document.onclick = function(evento) {
@@ -18,8 +24,9 @@ document.onclick = function(evento) {
 
     if (elemento.tagName === 'A' && elemento.hasAttribute('data-link')) {
         evento.preventDefault();
-        
+
         var pagina = elemento.getAttribute('href');
-        loadContent(pagina, 'content');
+
+        loadContent('html/pages/' + pagina, 'content');
     }
 };
